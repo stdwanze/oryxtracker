b7fd1875bad5dedec7c4b8ab085ae713f35ad70f
stdwanze <stdwanze@gmail.com>
2022-03-17 13:57:47 +0100

    added server

---
+++ server.js
@@ -0,0 +1,13 @@
+const polka = require('polka');
+const fs = require('fs');
+
+let port = 4000;
+polka().get("/csv", (req, res) => {
+    let csv = fs.readFileSync('lost.txt',"utf-8")
+    res.end(csv);
+
+})
+.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Running on localhost:`+port);
+  });
