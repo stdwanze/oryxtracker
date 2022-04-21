417153a232ebb1f3453a76983c4ba50f4be89f78
stdwanze <stdwanze@gmail.com>
2022-04-21 08:30:16 +0200

    adapted to 2 sites

--- getData.js
+++ getData.js
@@ -2,13 +2,26 @@ const https = require('https')
 
 
 
-
-
-
-
 function loadoryx(consumer){
 
-    const req = https.get('https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html?m=1', res => {
+    let first = "";
+    loadurl('https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html?m=1', function (html){
+        first = html;
+        loadurl("https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html", function (html2){
+            let ret = first + html2;
+            consumer(ret);
+        });
+
+    });
+
+}
+
+
+
+function loadurl(url, consumer){
+
+
+    const req = https.get(url, res => {
         let data = [];
 
         res.on('data', d => {
